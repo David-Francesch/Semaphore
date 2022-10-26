91d4ef171b4805af4844a360e71543eda4d33426
David-Francesch <david.francesch.colomer@gmail.com>
2022-10-26 13:16:56 +0200

    autobus

--- src/Visitantes.java
+++ src/Visitantes.java
@@ -6,19 +6,18 @@ public class Visitantes extends Thread {
     Semaphore entrada = null;
     Semaphore salida = null;
     Semaphore aforo = null;
+    Semaphore autobus = null;
 
-    public Visitantes(Semaphore entrada, Semaphore salida, Semaphore aforo) {
+    public Visitantes(Semaphore entrada, Semaphore salida, Semaphore aforo, Semaphore autobus) {
         super();
         this.entrada = entrada;
         this.salida = salida;
         this.aforo = aforo;
+        this.autobus = autobus;
 
     }
 
     public void run() {
-        // entra al museo
-        // ve los cuadros Sleep(random 1-5)
-        // sale del museo
         try {
             System.out.println(Thread.currentThread() + "Quiero entrar");
             entrada.acquire();
@@ -30,6 +29,13 @@ public class Visitantes extends Thread {
             System.out.println(Thread.currentThread() + "Quiero salir");
             salida.acquire();
             salida.release();
+
+            System.out.println(getId() + "Entro en el bus");
+            autobus.acquire();
+            // autobus.wait();
+            // autobus.notify();
+            autobus.release();
+            
         } catch (Exception e) {
 
         }
